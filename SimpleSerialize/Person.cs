4702d2b23017bff554afcddf8926f0852e119ce7
Bruce Howard <brucehoward@twc.com>
2020-09-09 16:55:19 -0400

    Add project files.

---
+++ SimpleSerialize/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSerialize
+{
+    [Serializable]
+    public class Person
+    {
+        // A public field.
+        public bool isAlive = true;
+
+        // A private field.
+        private int personAge = 21;
+        
+        // Public property/private data.
+        private string fName = string.Empty;
+        public string FirstName
+        {
+            get { return fName; }
+            set { fName = value; }
+        }
+    }
+}
---
+++ SimpleSerialize/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Gain access to the BinaryFormatter in mscorlib.dll.
+using System.Runtime.Serialization.Formatters.Binary;
+// Must reference System.Runtime.Serialization.Formatters.Soap.dll.
+using System.Runtime.Serialization.Formatters.Soap;
+// Defined within System.Xml.dll.
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace SimpleSerialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Object Serialization *****\n");
+            // Make a JamesBondCar and set state.
+            JamesBondCar jbc = new JamesBondCar();
+            jbc.canFly = true;
+            jbc.canSubmerge = false;
+            jbc.theRadio.stationPresets = new double[] { 89.3, 105.1, 97.1 };
+            jbc.theRadio.hasTweeters = true;
+            // Now save the car to a specific file in a binary format.
+            SaveAsBinaryFormat(jbc, "CarData.dat");
+            Console.ReadLine();
+
+            LoadFromBinaryFile("CarData.dat");
+            Console.ReadLine();
+
+            SaveAsSoapFormat(jbc, "CarData.soap");
+            Console.ReadLine();
+
+            SaveAsXmlFormat(jbc, "CarData.xml");
+            Console.ReadLine();
+
+            SaveListOfCars();
+            Console.ReadLine();
+
+            SaveListOfCarsAsBinary();
+            Console.ReadLine();
+        }
+
+        static void SaveListOfCarsAsBinary()
+        {
+            // Save ArrayList object (myCars) as binary.
+            List<JamesBondCar> myCars = new List<JamesBondCar>();
+            BinaryFormatter binFormat = new BinaryFormatter();
+            using (Stream fStream = new FileStream("AllMyCars.dat", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                binFormat.Serialize(fStream, myCars);
+            }
+            Console.WriteLine("=> Saved list of cars in binary!");
+        }
+
+        static void SaveListOfCars()
+        {
+            // Now persist a List<T> of JamesBondCars.
+            List<JamesBondCar> myCars = new List<JamesBondCar>();
+            myCars.Add(new JamesBondCar(true, true));
+            myCars.Add(new JamesBondCar(true, false));
+            myCars.Add(new JamesBondCar(false, true));
+            myCars.Add(new JamesBondCar(false, false));
+
+            using (Stream fStream = new FileStream("CarCollection.xml", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                XmlSerializer xmlFormat = new XmlSerializer(typeof(List<JamesBondCar>));
+                xmlFormat.Serialize(fStream, myCars);
+            }
+
+            Console.WriteLine("=> Saved list of cars!");
+        }
+
+        static void SaveAsXmlFormat(object objGraph, string fileName)
+        {
+            // Save object to a file named CarData.xml in XML format.
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(JamesBondCar));
+            using (Stream fStream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                xmlFormat.Serialize(fStream, objGraph);
+            }
+            Console.WriteLine("=> Saved car in XML format!");
+        }
+
+        static void SaveAsBinaryFormat(object objGraph, string fileName)
+        {
+            // Save object to a file named CarData.dat in binary.
+            BinaryFormatter binFormat = new BinaryFormatter();
+            using (Stream fStream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                binFormat.Serialize(fStream, objGraph);
+            }
+            Console.WriteLine("=> Saved car in binary format!");
+        }
+
+        static void LoadFromBinaryFile(string fileName)
+        {
+            BinaryFormatter binFormat = new BinaryFormatter();
+            // Read the JamesBondCar from the binary file.
+            using (Stream fStream = File.OpenRead(fileName))
+            {
+                JamesBondCar carFromDisk = (JamesBondCar)binFormat.Deserialize(fStream);
+                Console.WriteLine("Can this car fly? : {0}", carFromDisk.canFly);
+            }
+        }
+
+        static void SaveAsSoapFormat(object objGraph, string fileName)
+        {
+            // Save object to a file named CarData.soap in SOAP format.
+            SoapFormatter soapFormat = new SoapFormatter();
+            using (Stream fStream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                soapFormat.Serialize(fStream, objGraph);
+            }
+            Console.WriteLine("=> Saved car in SOAP format!");
+        }
+
+
+        //BinaryFormatter and SoapFormatter both implement IFormatter and IRemotingFormatter (which derives from IFormatter)
+        // So this method could support both
+        static void SerializeObjectGraph(IFormatter itfFormat, Stream destStream, object graph)
+        {
+            itfFormat.Serialize(destStream, graph);
+        }
+    }
+}
